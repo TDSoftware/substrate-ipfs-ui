ba05bb12aefbb69ae717af3aaf98767146513153
lasse78 <Linnamaeki>
2023-02-04 15:08:29 +0100

    toggle false on refresh

--- src/app/main.js
+++ src/app/main.js
@@ -14,6 +14,7 @@ let api;
 const defaultWsAddress = "ws://127.0.0.1:9944";
 
 async function main() {
+    document.getElementById("toggle").checked = false;
     await createNodeConnection(defaultWsAddress, api);
     await listenToBlocks();
     addListeners();
